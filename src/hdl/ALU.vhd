7a8045cf994a6467ac3dc52b2dc45d9131cd1de0
TaakoGarryl <c26Noah.Webster@afacademy.af.edu@users.noreply.github.com>
2024-05-05 22:44:14 -0600

    ALU up to C, but not connected

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -69,11 +69,34 @@ architecture behavioral of ALU is
         );
     end component twoscomp_decimal;
     
+    component Bitcompare is
+        port(
+            -- Switches
+            i_1, i_2: in std_logic_vector(7 downto 0);
+            -- LEDs
+            o_Sum_AND: out std_logic_vector(7 downto 0);
+            o_Sum_OR: out std_logic_vector(7 downto 0)
+          --  o_CarryOut: out std_logic
+        );
+    end component Bitcompare;
+    
+    component shifty is
+     Port(
+        i_A : std_logic_vector( 7 downto 0);
+        i_B : std_logic_vector( 7 downto 0);
+        i_D : std_logic;
+        o_shifted : std_logic_vector( 7 downto 0)
+     );
+    end component shifty;
+    
    signal w_operand1 : std_logic_vector( 7 downto 0);
    signal w_operand2_pos : std_logic_vector( 7 downto 0);
    signal w_operand2_neg : std_logic_vector( 7 downto 0);
-   signal w_result : std_logic_vector( 7 downto 0);
+   signal w_and : std_logic_vector( 7 downto 0);
+   signal w_or : std_logic_vector( 7 downto 0);
+   signal w_add : std_logic_vector( 7 downto 0);
    signal w_neg : std_logic;
+   signal w_shifted : std_logic_vector( 7 downto 0);
    signal w_flag_C :std_logic;
    signal w_flag_Z : std_logic;
 begin
@@ -81,7 +104,7 @@ begin
 	 FA_inst : FullAdder port map (
            i_1 => w_operand1,
            i_2 => w_operand2_pos,
-           o_Sum => w_result, -- added value
+           o_Sum => w_add, -- added value
            o_CarryOut => w_flag_C -- Set the carry-out flag
        );
 	
@@ -93,23 +116,54 @@ begin
               o_tens => open,     -- Unused for now
               o_ones => open      -- Unused for now
           );
+          
+     AND_OR_inst: Bitcompare port map (
+        i_1 => w_operand1,
+        i_2 => w_operand2_pos,
+        o_Sum_AND => w_and,
+        o_Sum_OR => w_or
+        );
+            
+	shift_inst : shifty port map(
+	   i_A => w_operand1,
+	   i_B => w_operand2_pos,
+	   i_D => i_op(2),
+	   o_shifted => w_shifted
 	
+	);
 	-- CONCURRENT STATEMENTS ----------------------------
 	
 	
-	add_sub : process (i_op, i_B, i_A)
+	add_sub_AO : process (i_op, i_B, i_A)
         begin
-        if i_op(1) = '1' then
-             w_operand2_pos  <= w_operand2_neg;
-             if i_A = i_B then
-                w_flag_Z <= '1';
-             end if;
-        else
-            w_operand2_pos <= i_B;
-        end if;
-    end process add_sub;
+        if i_op = "010" then  -- Subtract
+                w_operand2_pos  <= w_operand2_neg;
+                w_add <= o_op_result;
+                if i_A = i_B then
+                    w_flag_Z <= '1';
+                else
+                    w_flag_Z <= '0';
+                end if;
+                
+            elsif i_op = "011" then -- Shift left
+                w_shifted <= o_op_result;
+                
+            elsif i_op = "100" then -- Shift right
+                w_shifted <= o_op_result;
+                
+            elsif i_op = "101" then -- Bitwise AND
+                w_add <= o_op_result;
+                
+            elsif i_op = "110" then -- Bitwise OR
+                w_or <= o_op_result;
+                
+            else  -- Default case: Addition
+                w_operand2_pos <= i_B;
+                w_add <= o_op_result;
+            end if;
+    end process add_sub_AO;
         
     w_flag_C <= o_flag_C;
-    w_result <= o_op_result;
+    w_add <= o_op_result;
     w_flag_Z <= o_flag_Z;
 end behavioral;
